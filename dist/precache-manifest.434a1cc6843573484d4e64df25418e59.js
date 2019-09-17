23f4c198826d5ec2637efb0c9e023ee81fb6dd3a
guobin <13057620951@163.com>
2019-09-17 19:39:24 +0800

    1

---
+++ dist/precache-manifest.434a1cc6843573484d4e64df25418e59.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4ef4842c18f44b7bfeabbfefa0ea69d",
+    "url": "./index.html"
+  },
+  {
+    "revision": "7d1e6e95308e2ab0385d",
+    "url": "./static/css/2.368bef3c.chunk.css"
+  },
+  {
+    "revision": "882d528a61ab509c89c0",
+    "url": "./static/css/main.30622e40.chunk.css"
+  },
+  {
+    "revision": "7d1e6e95308e2ab0385d",
+    "url": "./static/js/2.5c4623d3.chunk.js"
+  },
+  {
+    "revision": "882d528a61ab509c89c0",
+    "url": "./static/js/main.f347f795.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "ad6c9e29128435c65542101ffc895e8f",
+    "url": "./static/media/1.ad6c9e29.jpg"
+  }
+]);
--- dist/service-worker.js
+++ dist/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.0/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.5d269eb809421279a12d8d96d8f2c997.js"
+  "./precache-manifest.434a1cc6843573484d4e64df25418e59.js"
 );
 
 self.addEventListener('message', (event) => {
