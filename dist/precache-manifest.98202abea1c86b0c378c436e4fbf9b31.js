bf486b6c0529c8ba7e27252711d5d21eb4550f12
guobin <13057620951@163.com>
2019-09-24 13:45:47 +0800

    1

---
+++ dist/precache-manifest.98202abea1c86b0c378c436e4fbf9b31.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae20924c47b84b076dc5541dcbaa366e",
+    "url": "./index.html"
+  },
+  {
+    "revision": "15e3784b8ecf92988c7b",
+    "url": "./static/css/2.cc5d590a.chunk.css"
+  },
+  {
+    "revision": "489615eaea8bfd3dc286",
+    "url": "./static/css/main.ea5ec214.chunk.css"
+  },
+  {
+    "revision": "15e3784b8ecf92988c7b",
+    "url": "./static/js/2.669a5c65.chunk.js"
+  },
+  {
+    "revision": "489615eaea8bfd3dc286",
+    "url": "./static/js/main.3c296095.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "ad6c9e29128435c65542101ffc895e8f",
+    "url": "./static/media/1.ad6c9e29.jpg"
+  }
+]);
